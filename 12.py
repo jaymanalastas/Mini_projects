754ba8994bb1246c94e2be9c89b3c82503a65942
Jay <jay.manalastas@outlook.com>
2019-05-17 12:46:00 -0400

    5th commit

--- 12.py
+++ 12.py
@@ -4,4 +4,13 @@
 letter_s = ''
 for row in range(7):
     for col in range(7):
-        if((row === 0 or row == 3 or row == 6) and col>0 and col<7):
+        if((row == 0 or row == 3 or row == 6) and col>0 and col<7):
+            letter_s = letter_s + '*'
+        elif ((row == 1 or row == 2) and col == 1):
+            letter_s = letter_s + '*'
+        elif ((row == 4 or row == 5) and col ==6):
+            letter_s = letter_s + '*'
+        else:
+            letter_s = letter_s + ' '
+    letter_s = letter_s + '\n'
+print(letter_s)
